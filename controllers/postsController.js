4588339266f735f31ad5b2f7d83b5bb6897f0057
Lucia-Susanna <lucia_susanna@hotmail.it>
2025-02-05 18:07:45 +0100

    feat: milestone 3

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -29,7 +29,13 @@ const modify = (req, res) => {
 }
 
 const destroy = (req, res) => {
-  res.send(`elimino post con id ${req.params.id}`)
+  const id = req.params.id;
+  const sql = 'DELETE FROM posts WHERE id = ?'
+
+  connection.query(sql, [id], (err) => {
+    if (err) return res.status(500).json({ error: 'eliminazione pizza fallita' })
+    res.sendStatus(204)
+  })
 }
 
 module.exports = {
